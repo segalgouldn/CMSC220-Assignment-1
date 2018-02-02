a04e3e583a971584b46dc018821d3c40c905f59e
Noah Segal-Gould <ns2349@bard.edu>
2018-02-02 10:05:39 -0500

    Update Segal-Gould_Noah_Assigment1Code.py

--- Segal-Gould_Noah_Assigment1Code.py
+++ Segal-Gould_Noah_Assigment1Code.py
@@ -4,12 +4,14 @@
 # Lab Assignment 1
 # My code is based in part on work done collectively in class.
 
+# I added some more sequences for testing purposes.
 sequence_1 = "CTTTCGCAATGATAC"
 sequence_2 = "GAAAGCGTTACTATG"
 sequence_3 = "UAGGCCAUCCGUAAC"
 sequence_4 = "CTTTCGCAAZZZZZZ"
 
 
+# I modified the validation step.
 def ligase(seq_1, seq_2):
     sum_sequence = seq_1 + seq_2
     if all(n in "ATCG" for n in sum_sequence):
@@ -17,7 +19,8 @@ def ligase(seq_1, seq_2):
     else:
         raise Exception("DNA sequence invalid.")
     
-    
+
+# The function accepts both RNA and DNA, and also replaces non-nucleotides with "N"
 def reverseTransDNA(seq):
     seq = seq.upper()
     if "U" not in seq:
@@ -28,14 +31,16 @@ def reverseTransDNA(seq):
         dna_sequence = seq.replace("U", "T")
         translation_dictionary = {"A": "T", "T": "A", "C": "G", "G": "C"}
         return seq, "".join([translation_dictionary[n] if n in "ATCG" else "N" for n in reversed(dna_sequence)])
-     
-        
+
+    
+# This function splits sequences in two parts at the chosen index.
 def nuclease(seq, cutpos):
     cut_1 = seq[:cutpos]
     cut_2 = seq[cutpos:]
     return cut_1, cut_2
 
 
+# Tests
 print("*"*10 + " TESTS " + "*"*10)
 print(sequence_1 + " + " + sequence_2 + " => " + ligase(sequence_1, sequence_2))
 print("DNA to RNA of " + sequence_1 + ": " + str(reverseTransDNA(sequence_1)))
